698c45aa67ea1ddb8e81ed02d38482fe6f7bbc69
EmilyImunde <imunde.emily@yahoo.com>
2026-03-15 15:28:17 +0100

    ELT pipeline till step 15

--- src/load_postgres.py
+++ src/load_postgres.py
@@ -52,3 +52,118 @@ def load_sales_report(records):
     connection.commit()
     cursor.close()
     connection.close()
+
+
+def load_raw_customers(customers):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("DELETE FROM raw_customers;")
+
+    insert_query = """
+        INSERT INTO raw_customers (
+            customer_id,
+            customer_name,
+            city,
+            signup_date
+        )
+        VALUES (%s, %s, %s, %s);
+    """
+
+    for customer in customers:
+        cursor.execute(
+            insert_query,
+            (
+                customer.get("customer_id"),
+                customer.get("customer_name"),
+                customer.get("city"),
+                customer.get("signup_date")
+            )
+        )
+
+    connection.commit()
+    cursor.close()
+    connection.close() 
+
+
+
+def load_raw_orders(orders):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("DELETE FROM raw_orders;")
+
+    insert_query = """
+        INSERT INTO raw_orders (
+            order_id,
+            customer_id,
+            product_id,
+            quantity,
+            order_date
+        )
+        VALUES (%s, %s, %s, %s, %s);
+    """
+
+    for order in orders:
+        cursor.execute(
+            insert_query,
+            (
+                order.get("order_id"),
+                order.get("customer_id"),
+                order.get("product_id"),
+                order.get("quantity"),
+                order.get("order_date")
+            )
+        )
+
+    connection.commit()
+    cursor.close()
+    connection.close()   
+
+
+def load_raw_products(products):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("DELETE FROM raw_products;")
+
+    insert_query = """
+        INSERT INTO raw_products (
+            product_id,
+            product_name,
+            category,
+            price
+        )
+        VALUES (%s, %s, %s, %s);
+    """
+
+    for product in products:
+        cursor.execute(
+            insert_query,
+            (
+                product.get("product_id"),
+                product.get("product_name"),
+                product.get("category"),
+                product.get("price")
+            )
+        )
+
+    connection.commit()
+    cursor.close()
+    connection.close()    
+
+from pathlib import Path
+
+
+def run_sql_file(file_path):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    sql_path = Path(file_path)
+    sql_content = sql_path.read_text(encoding="utf-8")
+
+    cursor.execute(sql_content)
+
+    connection.commit()
+    cursor.close()
+    connection.close()
